e9765cffcefdcc1e6e875ed16090d5da8b25effe
KATIA16 <80964779+KATIA16@users.noreply.github.com>
2021-10-11 15:44:27 -0300

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,45 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main{
+    static int x = 15;
+    
+	public static void main(String[] args) {
+	  
+	   String str = "A";  
+	   double y = 3.14;  
+	   int z = (int)3.14;
+	   String b = " A";
+
+	System.out.println("O valor de x é:  " + x);	
+	System.out.println("O valor de y é = " + 3.14);
+	System.out.println("Minha string = " + str);    
+	System.out.println("O valor de z é " + z); 
+	System.out.println("O valor de b é: " + str);
+	 
+	 
+	 	}
+
+    
+    
+}
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
